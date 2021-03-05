09fb6986b0545a46565f3a15eb80d2c66c8cfb9d
eduardo divardin <luisedivardin@live.com>
2021-03-05 16:15:13 -0300

    Adicionar arquivos de projeto.

---
+++ GFT INI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int formula, contador, numero;
+
+
+            Console.Write("Inicio do intervalo: ");
+            numero = Int32.Parse(Console.ReadLine());
+
+            
+            for (contador = 1; contador <= 10; ++contador)
+            {
+                formula = numero * contador;
+                Console.WriteLine(numero + " X " + contador + " = " + formula);
+                
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
